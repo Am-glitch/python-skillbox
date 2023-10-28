6470860d00a4e5cc1bbcec98d6b3281e770a263d
Vladislav <VladislavAndreev1337@yandex.ru>
2023-10-28 10:32:03 +0500

    finished module 10

--- module 10 cycles in cycles/task1.py
+++ module 10 cycles in cycles/task1.py
@@ -1,1 +1,4 @@
-# не сделано
+for i in range(6):
+    for j in range(0, 12, 2):
+        print(i + j, end='\t')
+    print()
--- module 10 cycles in cycles/task8.py
+++ module 10 cycles in cycles/task8.py
@@ -1,1 +1,13 @@
-# не сделано
+N = int(input("Введите число N: "))
+    
+for i in range(1, N + 1):
+    for j in range(N, N - i, -1):
+        print(j, end="")
+        
+    for _ in range(2 * (N - i)):
+        print(".", end="")
+
+    for j in range(N - i + 1, N + 1):
+        print(j, end="")
+    print()  
+
